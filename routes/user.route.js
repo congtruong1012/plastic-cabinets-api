7fbb5d304080daffc6639b4c421db8b3ef7f0f49
congtruong <congtruong1012@gmail.com>
2022-07-29 16:45:37 +0700

    updae

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,7 +4,11 @@ const { verifyRoleAdmin } = require("../middleware/verify");
 
 const router = express.Router();
 
-router.post("/create", verifyRoleAdmin, userController.create);
+router.post(
+  "/create",
+  // verifyRoleAdmin,
+  userController.create
+);
 router.get("/list", userController.get);
 router.post("/set-role", userController.setRoleMember);
 router.post("/remove-role", userController.removeRoleMember);
