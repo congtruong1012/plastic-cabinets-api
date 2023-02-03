4e481db66227dc344dce3737a61a5ae174e152b6
congtruong1012 <55123753+congtruong1012@users.noreply.github.com>
2023-02-03 09:43:07 +0700

    Update connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log('db', process.env.DB_URL)
     mongoose.connect(
       process.env.DB_URL,
       {
